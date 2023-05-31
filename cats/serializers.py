3b972f8b49aa05d8d51ab269bf918bd87ef26ab2
PeterFVin <peterwinogradov@gmail.com>
2023-05-31 17:21:21 +0300

    v1

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,82 @@
+import datetime as dt
 from rest_framework import serializers
+import webcolors
 
-from .models import Cat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
+
+
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+    
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id',
+                  'name',
+                  'color',
+                  'birth_year',
+                  'owner',
+                  'achievements',
+                  'age', 
+                  'color')
+
+    def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+
+        # Иначе делаем следующее:
+        # Уберём список достижений из словаря validated_data и сохраним его
+        achievements = validated_data.pop('achievements')
+        # Сначала добавляем котика в БД
+        cat = Cat.objects.create(**validated_data)
+        # А потом добавляем его достижения в БД
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            # И связываем каждое достижение с этим котиком
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color') 
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,12 +2,17 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register(r'mycats', LightCatViewSet)
+router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
